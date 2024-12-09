52aaa1a7638a30dc201d9523a4636ded478a063b
Thorbjørn Larsen <thla19ac@student.cbs.dk>
2024-12-09 15:37:15 +0100

    Python Exam 9/12

---
+++ Data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+data_cov=pd.read_excel(r"/Users/osito/Library/CloudStorage/OneDrive-CBS-CopenhagenBusinessSchool/Masters/3. Semester/Library/PyExam/covariance_matrix.xlsx")
+data_init=pd.read_excel(r"/Users/osito/Library/CloudStorage/OneDrive-CBS-CopenhagenBusinessSchool/Masters/3. Semester/Library/PyExam/init_values.xlsx")
+
+
+
+
